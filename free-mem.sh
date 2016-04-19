338339dcd6e634418495761a673ba8895a8f0cdb
Jacob Israel <jacobnix@gmail.com>
2016-04-19 14:13:00 -0500

    Create free-mem.sh

---
+++ free-mem.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Jacob 28/Abril/2014
+
+DATE="`date`"
+
+LOGFILE=/$HOME/myscripts/free-mem.sh.log
+
+echo "============================ $DATE ==================================" >> $LOGFILE
+echo "Your Server Free Memory Script" >> $LOGFILE
+echo "Sync.." >> $LOGFILE
+sync
+echo "Free Drop Caches.." >> $LOGFILE
+echo "3" > /proc/sys/vm/drop_caches
+free -g >> $LOGFILE
